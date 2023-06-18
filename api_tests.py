43de88486ef86633b1459c8dd61345a57e44ba78
Grisha Lavrov <65803914+GLec1er@users.noreply.github.com>
2023-06-18 23:05:57 +0300

    Update api_tests.py

--- api_tests.py
+++ api_tests.py
@@ -28,3 +28,50 @@ def test_post_post():
 def test_delete_post():
     response = requests.delete(f"{API_URL}/posts/1")
     assert response.status_code == 200
+    # Добавьте дополнительные проверки для ответа, если необходимо
+
+def test_get_post_by_id():
+    response = requests.get(f"{API_URL}/posts/1")
+    assert response.status_code == 200
+    # Добавьте дополнительные проверки для ответа, если необходимо
+
+def test_get_comments_by_post_id():
+    response = requests.get(f"{API_URL}/posts/1/comments")
+    assert response.status_code == 200
+    # Добавьте дополнительные проверки для ответа, если необходимо
+
+def test_update_post():
+    payload = {
+        "title": "foo",
+        "body": "bar",
+    }
+    response = requests.put(f"{API_URL}/posts/1", json=payload)
+    assert response.status_code == 200
+    # Добавьте дополнительные проверки для ответа, если необходимо
+
+def test_patch_post():
+    payload = {
+        "title": "foo",
+    }
+    response = requests.patch(f"{API_URL}/posts/1", json=payload)
+    assert response.status_code == 200
+    # Добавьте дополнительные проверки для ответа, если необходимо
+
+def test_get_users():
+    response = requests.get(f"{API_URL}/users")
+    assert response.status_code == 200
+    # Добавьте дополнительные проверки для ответа, если необходимо
+
+def test_get_user_by_id():
+    response = requests.get(f"{API_URL}/users/1")
+    assert response.status_code == 200
+    # Добавьте дополнительные проверки для ответа, если необходимо
+
+def test_post_user():
+    payload = {
+        "name": "John Doe",
+        "username": "johndoe",
+        "email": "john@example.com"
+    }
+    response = requests.post(f"{API_URL}/users", json=payload)
+    assert response.status_code == 201
